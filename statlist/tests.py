ea182c003bcdbb1ef8ee1a195b0a4f8a6775e00e
ISAAC LARYEA <i.laryea.21@abdn.ac.uk>
2022-03-30 20:48:32 +0000

    final tests

--- statlist/tests.py
+++ statlist/tests.py
@@ -2,6 +2,7 @@ from django.test import TestCase, SimpleTestCase,Client
 from statlist.views import home, profile, details, country, club
 from django.urls import reverse, resolve
 # Create your tests here.
+
 class TestUrls(SimpleTestCase):
 
     def test_home_url_resolves(self):
@@ -27,3 +28,27 @@ class TestOtherUrls(TestCase):
         client = Client()
         response = client.get('/')
         self.assertEqual(response.status_code, 200)
+
+
+class TestViews(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+        self.profile_url = reverse('profile')
+        self.club_url = reverse('club')
+        self.country_url = reverse('country')
+
+    def test_profile_GET(self):
+        response = self.client.get(self.profile_url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'statlist/profile.html')
+
+    def test_club_GET(self):
+        response = self.client.get(self.club_url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'statlist/club.html')
+
+    def test_country_GET(self):
+        response = self.client.get(self.country_url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'statlist/country.html')
