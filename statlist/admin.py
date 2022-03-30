eb4a282747bff3fa9db063831f77e5d91e76b1f1
SAMUEL OLANIYAN <s.olaniyan.21@abdn.ac.uk>
2022-03-30 20:35:22 +0000

    registered the models for view on admin

--- statlist/admin.py
+++ statlist/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import Player, Club, Country, TransferMarket, Skills
 # Register your models here.
+admin.site.register(Player)
+admin.site.register(Club)
+admin.site.register(Country)
+admin.site.register(TransferMarket)
+admin.site.register(Skills)
