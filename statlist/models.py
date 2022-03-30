7dd047e7957cee25730a279e4b187f53acaa0952
ISAAC LARYEA <i.laryea.21@abdn.ac.uk>
2022-03-30 20:22:24 +0000

    created table for country

--- statlist/models.py
+++ statlist/models.py
@@ -1,3 +1,16 @@
+from operator import truediv
 from django.db import models
 
 # Create your models here.
+
+
+class Country(models.Model):
+    countryID = models.AutoField(primary_key=True)
+    countryName = models.CharField(max_length=200)
+    countryFlag = models.URLField(max_length=1000)
+
+    def __str__(self):
+        return f"{self.countryName}"
+
+    class Meta:
+        ordering = ('countryName',)
