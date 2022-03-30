3a25acb654be534dc3e39324351cdb7f8b0cbf92
ISAAC LARYEA <i.laryea.21@abdn.ac.uk>
2022-03-30 20:41:49 +0000

    simple tests

--- statlist/tests.py
+++ statlist/tests.py
@@ -1,3 +1,21 @@
-from django.test import TestCase
-
+from django.test import TestCase, SimpleTestCase,Client
+from statlist.views import home, profile, details, country, club
+from django.urls import reverse, resolve
 # Create your tests here.
+class TestUrls(SimpleTestCase):
+
+    def test_home_url_resolves(self):
+        url = reverse('home')
+        self.assertEqual(resolve(url).func, home)
+
+    def test_profile_url_resolves(self):
+        url = reverse('profile')
+        self.assertEqual(resolve(url).func, profile)
+
+    def test_club_url_resolves(self):
+        url = reverse('club')
+        self.assertEqual(resolve(url).func, club)
+
+    def test_country_url_resolves(self):
+        url = reverse('country')
+        self.assertEqual(resolve(url).func, country)
