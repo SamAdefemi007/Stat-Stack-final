9df240b9251451760be98ec7520d9d1f3a33f8c2
SAMUEL OLANIYAN <s.olaniyan.21@abdn.ac.uk>
2022-03-30 23:00:48 +0000

    heroku deployment

--- StatStack/settings.py
+++ StatStack/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'StatStack.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['/templates/statlist'],
+        'DIRS': [os.path.join(BASE_DIR, 'templates'), os.path.join(BASE_DIR, 'templates/statlist'),],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- statlist/urls.py
+++ statlist/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
 from . import views
 
-app_name = 'statlist'
 urlpatterns = [
     path('', views.home, name="home"),
     path('profile', views.profile, name="profile"),
