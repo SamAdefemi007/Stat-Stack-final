7c2542c8798e13cead9b0737ea9d4b83419c80b6
ISAAC LARYEA <i.laryea.21@abdn.ac.uk>
2022-03-30 20:06:15 +0000

    added profile functions to views

--- statlist/views.py
+++ statlist/views.py
@@ -30,3 +30,15 @@ def club(request):
                 playerClub__clubName=clubname).order_by("-skills__rating")
 
     return render(request, 'statlist/club.html', {'clubs': sorted(clubObj), 'players': playerobj, 'clubname': clubname, 'clublogo': clublogo})
+
+
+    def profile(request):
+    try:
+        playerobj = Player.objects.all().order_by("-skills__rating")
+    except ObjectDoesNotExist:
+        print("the player object does not exist or could not be fetched from the database")
+    else:
+        paginator = Paginator(playerobj, 15)
+        page_num = request.GET.get("page")
+        page_obj = paginator.get_page(page_num)
+    return render(request, 'statlist/profile.html', {'players': page_obj})
